7b268f33f95ebe9d693853910f5d99daff8d103f
Jacob Krall <jacob@jkrall.net>
2020-06-27 20:22:46 -0400

    ps2uart: show hex stream from ps/2 keyboard over uart tx

--- ps2uart.c
+++ ps2uart.c
@@ -36,9 +36,50 @@ void puthex(char b) {
     putchar(HEXLUT[ (b & 0x0f)]);
 }
 
-void isr_ie0() __interrupt (IE0_VECTOR) {
-    P1=0x70;
+
+volatile unsigned char circular_ps2[0x20];
+volatile int ps2_write = 0;
+int ps2_read = 0;
+
+int decode_status = 0;
+char decoded = 0;
+
+void ps2_advance_reader() {
+    while(ps2_read != ps2_write) {
+        unsigned char n = circular_ps2[ps2_read];
+        ps2_read = (ps2_read + 1) & 0x1F;
+
+        switch(decode_status) {
+            case 0:
+                // start bit
+                decoded = 0;
+                decode_status++;
+                break;
+            case 9:
+                // parity bit
+                decode_status++;
+                break;
+            case 10:
+                // stop bit
+                decode_status = 0;
+                puthex(decoded);
+                break;
+            default:
+                decoded = decoded >> 1;
+                if (n) {
+                    decoded = decoded | 0x80;
+                }
+                decode_status++;
+                break;
+        }
+    }
 }
+
+void ps2_advance_writer() __interrupt (IE0_VECTOR) {
+    circular_ps2[ps2_write] = P3_3;
+    ps2_write = (ps2_write + 1) & 0x1F;
+}
+
 void main()
 {
     P1 = 0x00;
@@ -51,6 +92,7 @@ void main()
     IT0 = 1; // set external interrupt 0 to falling-edge trigger
 
     while(1) {
+        ps2_advance_reader();
     }
 }
 
